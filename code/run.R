c0b8e73d87a7efbf88e7f79d600903682ea18b96
mcsiple-NOAA <margaret.siple@noaa.gov>
2023-09-24 23:30:54 -0700

    initial commit-simplified report

--- code/run.R
+++ code/run.R
@@ -1,23 +1,16 @@
 #' ---------------------------
-#' Example 4: An annual report on snailfish in a parameterized year
-#' Emily.Markowitz AT noaa.gov
+#' Example: A one-pager on snailfish
 #' ---------------------------
 
 # Run report! ------------------------------------------------------------------
 
-for (report_year in c(2022, 2021, 1990)) { # A few years to cycle through
-  
+
+for (report_year in c(2022, 2021, 1990)) { # A few years to cycle throug
   # render the quarto document
   quarto::quarto_render(
     input = here::here("code4", "template4_parent.qmd"), 
     output_format = "html", 
     output_file = paste0("report_",report_year,".html"), 
     execute_params = list(report_year = report_year)) # subset data for the year
-  
-  # Create copy of files to the sub directory
-  file.copy(from = here::here(paste0("report_",report_year,".html")),
-            to = here::here("code4", paste0("report_",report_year,".html")), 
-            overwrite = TRUE)
-  file.remove(from = here::here(paste0("report_",report_year,".html")))
   
 }
