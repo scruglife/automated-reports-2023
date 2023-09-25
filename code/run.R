1c3196dc906808547444fe4c603ec8f38fc10057
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2023-09-25 09:06:10 -0700

    fixed typo in code/run.R

--- code/run.R
+++ code/run.R
@@ -4,13 +4,10 @@
 
 # Run report! ------------------------------------------------------------------
 
-
-for (report_year in c(2022, 2021, 1990)) { # A few years to cycle throug
-  # render the quarto document
-  quarto::quarto_render(
-    input = here::here("code4", "template4_parent.qmd"), 
+for (report_year in c(2022, 2021, 1990)) { # A few years to cycle through
+  quarto::quarto_render(  # render the quarto document
+    input = here::here("code", "_parent.qmd"), 
     output_format = "html", 
     output_file = paste0("report_",report_year,".html"), 
-    execute_params = list(report_year = report_year)) # subset data for the year
-  
+    execute_params = list(report_year = report_year)) # subset data for the iterating year
 }
