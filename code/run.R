b776530ca42c002f2d7acfc5e1e274e8c4fdb893
emilymarkowitz-noaa <emily.markowitz@noaa.gov>
2023-09-26 00:32:54 -0700

    fix figure child rendering (sort of) and other aesthetic fixes. Some additions to quarto presentation

--- code/run.R
+++ code/run.R
@@ -4,10 +4,22 @@
 
 # Run report! ------------------------------------------------------------------
 
-for (report_year in c(2022, 2021, 1990)) { # A few years to cycle through
+for (report_year in c(2010, 2021, 2022)) { # A few years to cycle through
   quarto::quarto_render(  # render the quarto document
     input = here::here("code", "_parent.qmd"), 
     output_format = "html", 
-    output_file = paste0("report_",report_year,".html"), 
+    output_file = paste0("report_",report_year,".html"),
     execute_params = list(report_year = report_year)) # subset data for the iterating year
+  
+  # Create copy of files to the sub directory
+  file.copy(from = here::here(paste0("report_",report_year,".html")),
+            to = here::here("code", paste0("report_",report_year,".html")), 
+            overwrite = TRUE)
+  file.remove(from = here::here(paste0("report_",report_year,".html")))
 }
+
+
+# copy to a "/docs" folder so it will be readable in the repo
+file.copy(from = here::here("code", paste0("report_",report_year,".html")),
+          to = here::here("docs", paste0("report_",report_year,".html")), 
+          overwrite = TRUE)
